985d1ecee56d30e7e1bed5f17d7f41ba99d20d76
B2MW <P5@Champion.local>
2014-10-07 13:11:58 -0500

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Bradley Walker on 10/7/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Bradley Walker on 10/7/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
